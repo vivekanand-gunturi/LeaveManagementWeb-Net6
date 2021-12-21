2e451668a6e3681b15658c76abc7f1284f41ac73
vivekanand-gunturi <knctme.vivekanand@gmail.com>
2021-12-21 17:26:43 +0530

    Added Database Tables and Migrations using the Entity Framework Core

---
+++ LeaveManagementApp/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementApp.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ LeaveManagementApp/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementApp.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+    }
+}
